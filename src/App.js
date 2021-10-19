009e636c328d4daa11a8591f359b05fc8dc20c46
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-10-19 15:40:06 -0300

    Exercise solved

--- src/App.js
+++ src/App.js
@@ -1,30 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Create component App
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.fetchDog = this.fetchDog.bind(this);
+    
+    // Set initial state
+    this.state = {
+      image: '',
+      loading: true,
+      urls: []
+    }
+  }
+
+  // Fetch API and update the state
+  async fetchDog() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const requestHeaders = { headers: { Accept: 'application/json' } }
+        const requestReturn = await fetch('https://dog.ceo/api/breeds/image/random', requestHeaders)
+        const requestObject = await requestReturn.json();
+        this.setState({
+          loading: false,
+          image: requestObject.message,
+        })
+      }  
+    )
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const { image, urls } = this.state;
+
+    if (image.includes('terrier')) {
+      return false
+    }
+    this.setState({
+      urls: [...image, nextState.message]
+    })
+    localStorage.setItem('urls', urls)
+    return true;
+  }
+
+  // Call fetch API after render
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  // Render according to the moment and the click
+  render() {
+    const { image, loading } = this.state;
+
+    return (
+      <div className="App">
+        { loading ? <span>Loading...</span> : <img src={ image } alt={ image } className="dog" /> }
+        <button type="button" onClick={ this.fetchDog }>New Dog</button>
+      </div>
+    );
+  }
 }
 
 export default App;
